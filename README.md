3dee0eaf13f054a734d810510d31f5908409bf03
maralgunV <144336649+maralgunV@users.noreply.github.com>
2023-09-15 15:16:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-document-editor-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a8caqr)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { DialogModule, TooltipModule } from '@syncfusion/ej2-angular-popups';
+
+import { DropDownButtonModule, SplitButtonModule } from '@syncfusion/ej2-angular-splitbuttons';
+
+import { CheckBoxModule, ButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import { SliderModule, NumericTextBoxModule, ColorPickerModule } from '@syncfusion/ej2-angular-inputs';
+
+import { ListViewAllModule } from '@syncfusion/ej2-angular-lists';
+
+import { DropDownListModule, ComboBoxModule, DropDownListAllModule, MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
+
+import { DocumentEditorAllModule, DocumentEditorContainerAllModule } from '@syncfusion/ej2-angular-documenteditor';
+
+import { ToolbarModule, TabModule } from '@syncfusion/ej2-angular-navigations';
+
+import { HttpModule } from '@angular/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+import { AppComponent } from '../app.component';
+@NgModule({ declarations: [ AppComponent ], imports: [ FormsModule, ToolbarModule, DropDownListAllModule, ColorPickerModule, SplitButtonModule,       ComboBoxModule, TabModule, DocumentEditorAllModule, DocumentEditorContainerAllModule, DropDownListModule, SliderModule, TooltipModule, NumericTextBoxModule,       CheckBoxModule, ButtonModule, DropDownButtonModule, DialogModule, BrowserModule, MultiSelectAllModule, ListViewAllModule], providers: [], bootstrap: [AppComponent]
+})
+export class AppModule { }
